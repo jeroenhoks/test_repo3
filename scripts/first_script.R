387c78cc19e160b76f316aa98d34aea276a6bbb5
Jeroen Hoks <j.d.hoks@student.rug.nl>
2023-11-13 12:21:00 +0100

    fig01 produced + restore added

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -5,10 +5,18 @@ library(renv)
 # restore the versions of the  packages used to decelop this project
 renv::restore()
 
+# remember to use pull in Git tab to get changes from github 
+
+# load libraries 
+library(tidyverse)
+
+
 x <- c(1,2,3,4,5)
 y <- c(1,4,3,6,9)
 data <- data.frame(x,y)
 data 
+
+data |> 
 
 
 
